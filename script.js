cf1a1908258a99e6201936323c72b5e00b0caa1d
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-16 12:43:31 +0300

    new tak

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ function amountOfPages(summary){
         summ += String(i).length;
     }
     return --i;
+};
+
+function isPangram(string) {
+    let str = string.trim().split(' ').join('').toLowerCase().split('');
+    return (new Set(str).size == 26);
+};
+
+function deepCount(a){
+    let count = 0;
+    for(let value of a){
+        count++;
+        if(Array.isArray(value))
+            count += deepCount(value);
+    }
+    return count;
 }

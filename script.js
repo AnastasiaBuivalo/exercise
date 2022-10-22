1a33bbde113a544c89daf212253ade370fbd142e
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-22 09:33:35 +0300

    ex20

--- script.js
+++ script.js
@@ -21,3 +21,78 @@ function deepCount(a){
     }
     return count;
 }
+
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showGoodFilms(arr) {
+    return arr.filter(item => item.rating >= 8);
+}
+
+console.log(showGoodFilms(films));
+
+function showListOfFilms(arr) {
+    return arr.reduce((sum, item) => `${typeof(sum) === 'object' ? sum.name:sum}, ${item.name}`)
+}
+
+console.log(showListOfFilms(films));
+
+
+
+
+function setFilmsIds(arr) {
+    return arr.map((item, count) => {
+        item.id = count++
+        return item;
+    });
+}
+
+console.log(setFilmsIds(films));
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    return arr.every(item => item.id != 'undefined')
+}
+
+console.log(checkFilms(films));
+
+
+
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount > 0).reduce((sum, curr) => sum + curr.amount, 0)
+};
+
+console.log(getPositiveIncomeAmount(funds));
+
+const getTotalIncomeAmount = (data) => {
+    return data.reduce((sum, curr) => sum + curr.amount, 0)
+};
+
+console.log(getTotalIncomeAmount(funds));
